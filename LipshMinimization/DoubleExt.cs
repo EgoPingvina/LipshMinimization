d7973dabd1b9792f8abf4bc2923df91ebf127174
Aleksey <aleks-herou@mail.ru>
2019-05-05 22:46:02 +0300

    ToRadians extension for double

---
+++ LipshMinimization/DoubleExt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LipshMinimization
+{
+    public static class DoubleExt
+    {
+        public static double ToRadians(this double angle)
+            => (Math.PI / 180) * angle;
+    }
+}
