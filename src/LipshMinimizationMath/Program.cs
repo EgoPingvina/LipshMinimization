b4bd68d76975005e03654546703c61a3ac8644fb
Aleksey <aleks-herou@mail.ru>
2019-05-14 09:35:02 +0300

    Refactoring +XUnit

--- src/LipshMinimizationMath/Program.cs
+++ src/LipshMinimizationMath/Program.cs
@@ -1,163 +1,10 @@
-﻿namespace LipshMinimizationMath
+﻿using System;
+using System.Diagnostics;
+
+namespace LipshMinimization.ELipschitzMath
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Linq;
-
-    public static class MathStrategy
+    public static class ELipschitzMath
     {
-        public static void Main(string[] args)
-        {
-            double a    = -1
-                , b     = 1 
-                , d     = -Math.PI/2.0
-                , c     = Math.PI/2.0
-                , e     = 0.001
-                , e2    = 0.01
-                , L     = 1.0 / (4.0 * e) + 1;
-
-            // Рассматриваемая функция
-            double F(double x, double y)
-                => Math.Abs(x) + Math.Sqrt(Math.Abs(Math.Sin(y)));
-
-            var result  = UniformSearchByBiryukov(
-                    F,
-                    a, b,       // [a;b]
-                    d, c,       // [d;c]
-                    L,          // L=L(e)=1/(4e)
-                    e, e2);     // e, e*
-
-            Console.WriteLine($"e={e}, e2={e2}");
-            Console.WriteLine($"Модернизированный метод равномерного перебора для эпсилен-липшицевых функций(Бирюков edition):\nL={result.L}, hx={result.hx.ToString("F6")}, hy={result.hy.ToString("F6")}, x={result.x.ToString("F6")}, y={result.y.ToString("F6")}, F={result.F.ToString("F6")}, n={result.n}, m={result.m}, t={result.time}\n{result}");
-            Console.ReadKey();
-        }
-
-        /// <summary>
-        /// Метод ломанных(Пиявского)
-        /// </summary>
-        public static double PolygonalMethod(Func<double, double> F, double a, double b, double L, double e, double? x0 = null)
-        {
-            // координаты оси абсцисс, в которых расположены вершины "шапочек" строящейся ломаной
-            // для удобства берём за основу ломаную с вершинами в концах рассматриваемого отрезка
-            var u = new List<double> { a, b };
-
-            // низины ломаной на текущей итерации построения
-            var k = new List<(double x, double y)>();
-
-            // координата оси абсцисс точки пересечения двух "шапочек"
-            double ui(double left, double right)
-                => (F(right) - F(left)) / (2 * L) + (left + right) / 2;
-
-            // функция "шапочки" с вершиной в точке (xi, F(xi))
-            double g(double x, double xi)
-                => F(xi) - L * Math.Abs(x - xi);
-
-            // полученная на текущей итерации ломаная
-            double p(double x)
-                => u.Select(t => g(x, t)).Max();
-
-            // точка, в которой p минимальна, станет новой вершиной
-            // изначально это либо заданная начальная точка, либо точка пересечения "шапочек" построенных на концах отрезка
-            var temp = x0 ?? ui(a, b);
-            (double x, double y) xk = (temp, p(temp));
-
-            int position;
-            do
-            {
-                // выбранная на прошлой итерация низина становится очередной вершиной для строящейся ломаной
-                u.Add(xk.x);
-
-                u.Sort();
-
-                // удаляем переквалифицированную в вершины низину и добавляем низины-пересечения "шапочки" из неё с соседними
-                k.Remove(xk);
-                position = u.IndexOf(xk.x);
-                for (int i = 0; i < 2; i++)
-                {
-                    if (position == 0 && i == 1)
-                        continue;
-
-                    if (position == u.Count && i == 0)
-                        continue;
-
-                    temp = ui(u[position - i], u[position - i + 1]);
-                    k.Add((temp, p(temp)));
-                }
-
-                // выбираем новую минимальную низину
-                xk = k.OrderByDescending(intersection => intersection.y).Last();
-            } while (Math.Abs(F(xk.x) - xk.y) > e);
-
-            return xk.x;
-        }
-
-        #region Методы покрытий
-
-        /// <summary>
-        /// Метод перебора (метод равномерного поиска, перебор по сетке)
-        /// </summary>
-        public static double EnumerationMethod(Func<double, double> F, double a, double b, int n)  // из википедии
-        {
-            var x = new List<double>();
-            double factor;
-
-            if (n / 2 == 0)
-            {
-                factor = (b - a) / (n / 2.0 + 1);
-                for (int i = 1; i <= n / 2; i++)
-                    x.Add(a + 2.0 * i * factor);
-            }
-            else
-            {
-                factor = (b - a) / (n + a);
-                for (int i = 1; i <= n; i++)
-                    x.Add(a + i * factor);
-            }
-
-
-            return x.Select(xi => (xi, F(xi))).OrderByDescending(pair => pair.Item2).Last().xi;
-        }
-
-        /// <summary>
-        /// Метод равномерного перебора(перебор на равномерной сетке)
-        /// </summary>
-        public static double UniformSearchMethod(Func<double, double> F, double a, double b, double L, double e)  // номер 2 в Васильеве(ПАССИВНЫЙ!!!)
-        {
-            double h = 2.0 * e / L,
-                ui = a + h / 2;
-            var u = new List<double> { ui };
-
-            var exitParam = b - h / 2;
-            while (!(ui < exitParam && exitParam <= (ui = ui + h)))
-                u.Add(ui);
-
-            u.Add(Math.Min(ui, b));
-
-            return u.Select(xi => (xi, F(xi))).OrderByDescending(pair => pair.Item2).Last().xi;
-        }
-
-        /// <summary>
-        /// Метод последовательного перебора(перебор на неравномерной сетке)
-        /// </summary>
-        public static double SerialEnumerationMethod(Func<double, double> F, double a, double b, double L, double e)   // номер 3 в Васильеве(Евтушенко, если не ошибаюсь)
-        {
-            double h = 2.0 * e / L,
-                ui = a + h / 2;
-            var u = new List<double> { ui };
-
-            double Fi()
-                => u.Select(x => F(x)).Min();
-
-            var exitParam = b - h / 2.0;
-            while (!(ui < exitParam && exitParam <= (ui = ui + h + (F(ui) - Fi()) / L)))
-                u.Add(ui);
-            
-            u.Add(Math.Min(ui, b));
-
-            return u.Select(xi => (xi, F(xi))).OrderByDescending(pair => pair.Item2).Last().xi;
-        }
-
         /// <summary>
         /// Модификация метода Евтушенко поиска глобального минимума для случая непрерывной на отрезке функции
         /// </summary>
@@ -174,7 +21,8 @@
         /// time-время, затраченное на выполнение поиска.</returns>
         public static (double x, double F, double n, long time) EvtushenkoMethodByArytunova(Func<double, double> F, double a, double b, double L, double e, double e2)    // модифицированный Арутюновой метод Евтушенко
         {
-            var sw = new Stopwatch();
+            // Таймер для приблизительного измерения производительности алгоритма
+            var sw      = new Stopwatch();
             sw.Start();
 
             double h    = 2.0 * (e2 - e) / L
@@ -236,6 +84,7 @@
         /// time-время, затраченное на выполнение поиска.</returns>
         public static (double L, double h, double x, double F, double n, long time) UniformSearchByBiryukov(Func<double, double> F, double a, double b, double L, double e, double e2)
         {
+            // Таймер для приблизительного измерения производительности алгоритма
             var sw      = new Stopwatch();
             sw.Start();
 
@@ -248,6 +97,7 @@
 
             while ((xi += h) < b)
             {
+                // если значение функции в текущей точке меньше последнего сохранённого - заменяем его
                 if ((tmp = F(xi)) < fMin)
                 {
                     xMin = xi;
@@ -263,15 +113,15 @@
 
         public static (double L, double hx, double hy, double x, double y, double F, double n, double m, long time) UniformSearchByBiryukov(Func<double, double, double> F, double a, double b, double d, double c, double L, double e, double e2)
         {
-            var sw = new Stopwatch();
+            // Таймер для приблизительного измерения производительности алгоритма
+            var sw      = new Stopwatch();
             sw.Start();
 
-            double m = Math.Ceiling((c - d) * L / (e2 - e)),
-                n=Math.Ceiling((b - a) * L / (e2 - e));
+            double m    = Math.Ceiling((c - d) * L / (e2 - e)), // число минимально необходимых пробных точек по оси Oy
+                n       = Math.Ceiling((b - a) * L / (e2 - e)); // число минимально необходимых пробных точек по оси Ox
 
-            // ToDo уточнить размер шага
-            double hx   = (b - a) / n  // шаг по оси Ox
-                , hy    = (c - d) / m  // шаг по оси Oy
+            double hx   = (e2 - e) / L  // шаг по оси Ox
+                , hy    = (e2 - e) / L  // шаг по оси Oy
                 , xi    = a             // координата на оси Ox, значение функции в которой рассмтаривается на текущей итерации
                 , yi    = d             // координата на оси Oy, значение функции в которой рассмтаривается на текущей итерации
                 , xMin  = xi            // координата оси Ox, на которой достигается лучшее приближение к глобальному минимуму функции на текущей итерации
@@ -285,6 +135,7 @@
 
                 while ((xi += hx) < b)
                 {
+                    // если значение функции в текущей точке меньше последнего сохранённого - заменяем его
                     if ((tmp = F(xi, yi)) < fMin)
                     {
                         xMin = xi;
@@ -298,153 +149,6 @@
             sw.Stop();
 
             return (L, hx, hy, xMin, yMin, fMin, n, m, sw.ElapsedMilliseconds);
-        }
-
-        public static double NoName(Func<double, double> F, double a, double b, double L, double e)    // номер 4 в Васильеве
-        {
-            int n = 0, k;
-            double hi = b - a;
-
-            // определяем минимальное целое n
-            while (hi / Math.Pow(2, n + 1) > e / L)
-                n++;
-
-            // вводим на отрезке [a;b] точки c[i,k]
-            var c = new List<List<double>>();
-            for (k = 0; k <= n; k++)
-            {
-                c.Add(new List<double>());
-
-                for (int i = 0; i <= Math.Pow(2, k); i++)
-                    c[k].Add(a + i * hi / Math.Pow(2, k));
-            }
-
-            // Удаляет интервал с левой границей element и шагом step на уровнях level и выше(дробления этого интервала),
-            // также убирает пустые уровни
-            int RemoveAndCleaning(int level, double element, double step)
-            {
-                c[level].Remove(element);
-
-                for (int i = level + 1; i < c.Count(); i++)
-                    c[i].RemoveAll(arg => arg >= element && arg < element + step);
-
-                int removing = 0;
-                for (int i = 0; i <= level; i++)
-                {
-                    if (c[i].Count <= 1)
-                    {
-                        c.Remove(c[i]);
-                        removing++;
-                    }
-                }
-
-                return level - removing;
-            }
-
-            double ai = a,
-                bi = b,
-                ui = (ai + bi) / 2,
-                fi = F(ui);
-
-            if (n == 0)
-                return fi;
-
-            int j = 1;  // рассматриваемый уровень
-            k = 0;      // левая граница рассматриваемого интервала
-            double minX = ui, temp;
-            while (c.SelectMany(arg => arg).Any())
-            {
-                ai = c[j][k];
-                bi = c[j][k + 1];
-
-                hi = (bi - ai) / 2;
-                ui = (ai + bi) / 2;
-
-                // F[k] = min{ F[k-1]; J(u[k]) }
-                temp = F(ui);
-                if (temp < fi)
-                {
-                    fi = temp;
-                    minX = ui;
-                }
-
-                // если j=n, то отрезок исключается из рассмотрения и на следующем шаге
-                // переходим к одному из оставшихся отрезков меньшего уровня
-                if (j == c.Count-1)
-                {
-                    j = RemoveAndCleaning(j, c[j][k], hi);
-
-                    j = 0;
-                    k = 0;
-                    
-                    continue;
-                }
-
-                // если выполняется условие, иссключаем интервал из рассмотрения, а в качестве следующего
-                // берём один из оставшихся отрезков самого меньшего уровня
-                if (fi <= F(ui) - L * hi + e)
-                {
-                    j = RemoveAndCleaning(j, c[j][k], hi);
-
-                    j = 0;
-                    k = 0;
-                    
-                    continue;
-                }
-
-                // если условие не выполняется берём одну из половин этого интервала
-                j++;
-                k = 0;
-            }
-
-            return minX;
-        }
-
-        #endregion
-
-        /// <summary>
-        /// Метод поиска глобального минимума(Стронгина)
-        /// </summary>
-        public static double GlobalMinimumSearch(Func<double, double> F, double a, double b, double e)
-        {
-            double v = 1.0, mk = 2.0;
-
-            List<double> u = new List<double> { a, b },
-                L = new List<double>();
-
-            var R = new List<(int, double)>();
-
-            int s = 0;
-            double d, Lmax;
-            do
-            {
-                // если вновь сюда вошли-сортируем, чтобы последняя добавленная точка нашла своё место;
-                // для первого же прохода ничего не изменится
-                u.Sort();
-
-                L.Clear();
-                for (int j = 1; j < u.Count; j++)
-                    L.Add(Math.Abs(F(u[j]) - F(u[j - 1])) / (u[j] - u[j - 1]));
-                Lmax = L.Max();
-
-                d = Lmax > 0 ? mk * Lmax : v;
-
-                R.Clear();
-                for (int j = 1; j < u.Count; j++)
-                    R.Add((
-                        j,
-                        d * (u[j] - u[j - 1])
-                            + Math.Pow(F(u[j]) - F(u[j - 1]), 2) / (d * (u[j] - u[j - 1]))
-                            - 2 * (F(u[j]) + F(u[j - 1]))));
-
-                s = R.OrderByDescending(r => r.Item2).First().Item1;
-
-                u.Add((u[s] + u[s - 1]) / 2.0 - (F(u[s]) - F(u[s - 1])) / (2.0 * d));
-            }
-            while (u[s] - u[s - 1] >= e);
-
-            // если условие выхода выполнилось => сортирвока н ебыла проведена => u(k+1) находится в конце коллекции
-            return u.Last();
         }
     }
 }
---
+++ src/LipshMinimizationTests/ELipschitzMathTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Xunit;
+
+using LipshMinimization.ELipschitzMath;
+using FluentAssertions;
+
+namespace LipshMinimizationTests
+{
+    public sealed class ELipschitzMathTests
+    {
+        private double L(double e)
+            => 1.0 / (4.0 * e) + 1;
+
+        [Theory]
+        [InlineData(-2,     3,  -Math.PI,        3 * Math.PI,    0.01,  0.1)]
+        [InlineData(-1,     1,  -Math.PI/2.0,    Math.PI/2.0,    0.01,  0.02)]
+        [InlineData(0,      1,  0,               Math.PI,        0.05,  0.1)]
+        public void TwoDimensionF1(double a, double b, double d, double c, double e, double e2)
+        {
+            // Рассматриваемая функция
+            double F(double x, double y)
+                => Math.Abs(x) + Math.Sqrt(Math.Abs(Math.Sin(y)));
+
+            double fMin = 0;
+
+            var result  = ELipschitzMath.UniformSearchByBiryukov(
+                    F,
+                    a, b,   // [a;b]
+                    d, c,   // [d;c]
+                    L(e),   // L=L(e)=1/(4e)
+                    e, e2); // e, e*
+
+            (result.F - fMin).Should().BeLessOrEqualTo(e2);
+        }
+
+        [Theory]
+        [InlineData(-Math.PI,       3 * Math.PI,    0.001,  0.01)]
+        [InlineData(-Math.PI/2.0,   Math.PI/2.0,    0.0005,  0.001)]
+        [InlineData(0,              Math.PI,        0.01,   0.1)]
+        public void OneDimensionF1(double a, double b, double e, double e2)
+        {
+            // Рассматриваемая функция
+            double F(double x)
+                => Math.Abs(x) + Math.Sqrt(Math.Abs(Math.Sin(x)));
+
+            double fMin = 0;
+
+            var result  = ELipschitzMath.UniformSearchByBiryukov(
+                   F,
+                   a, b,    // [a;b]
+                   L(e),    // L=L(e)=1/(4e)
+                   e, e2);  // e, e*
+
+            (result.F - fMin).Should().BeLessOrEqualTo(e2);
+        }
+    }
+}
